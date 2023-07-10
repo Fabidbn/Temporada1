10a349a7063bfdf0e5f0678bdec79c25f57ab064
Fabidbn <fabianadias.dev@gmail.com>
2023-07-10 19:51:26 -0300

    Adicionar arquivos de projeto.

---
+++ AcessoBancoDados/AcessoDadosSQLServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AcessoBancoDados
+{
+    public class AcessoDadosSQLServer
+    {
+        //Cria a conexão 
+
+        // Parametros que vão para o banco 
+
+        //Persistencia - Inserir, Alterar, Excluir
+
+        // Consultar registro do banco de dados
+    }
+}
---
+++ Apresentacao/Form1.cs
@@ -0,0 +1,10 @@
+namespace Apresentacao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
